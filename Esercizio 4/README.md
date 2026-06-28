bfcf89d03a6f79f99f84e7c651cf5be3bc5686ff
Chiara <chiarafioref@gmail.com>
2026-06-28 17:57:05 +0200

    implemented loop to print numbers from 0 to N based on user input

---
+++ Esercizio 4/README.md
@@ -0,0 +1,3 @@
+# ESERCIZIO
+
+Chiedi un numero N all'utente. Stampa i numeri da 0 a N usando un ciclo for.
---
+++ Esercizio 4/main.js
@@ -0,0 +1,7 @@
+console.log("script JS caricato");
+
+let number = Number(prompt("Inserisci un numero:"));
+
+for (let startNumber = 0; startNumber <= number; startNumber++) {
+    console.log(startNumber);
+}
