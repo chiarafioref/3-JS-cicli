5c5bd3bd725169e821b14427431cb8762cdd40ce
Chiara <chiarafioref@gmail.com>
2026-06-28 17:50:05 +0200

    added loop exercise with user-provided range input

---
+++ Esercizio 3/main.js
@@ -0,0 +1,9 @@
+console.log("script JS caricato");
+
+let startNumber = 0;
+let number = Number(prompt("Inserisci un numero:"));
+
+while (startNumber <= number) {
+    console.log(startNumber);
+    startNumber++;
+}
